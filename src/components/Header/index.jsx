fd575a9dcb8d3b72e5ae572ca314898ae2e13069
SvetlanAR30 <160256589+SvetlanAR30@users.noreply.github.com>
2024-03-13 20:50:47 +0400

    add basket styles

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,10 @@
-import React from "react"
+import React, {useState} from "react"
+import { BsBasket2Fill } from "react-icons/bs";
 import styles from "./Header.module.scss"
 
 export default function Header(){
+    let [cartOpen,setCartOpen]=useState(false);
+
     return(
         <header>
         <div>
@@ -11,6 +14,15 @@ export default function Header(){
                 <li>Контакты</li>
                 <li>Личный кабинет</li>
             </ul>
+            <BsBasket2Fill onClick={()=>setCartOpen(cartOpen= ! cartOpen)} className={`${styles.shopCartButton} ${cartOpen ? styles.active : ''}`}/>
+
+            {cartOpen &&(
+
+                <div className={styles.shopCart}>
+                    
+                </div>
+            )}
+
         </div>
         <div className={styles.presentation}></div>
         </header>
